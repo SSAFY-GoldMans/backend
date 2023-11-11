d3a5ed3031e0fc9f3e00751bdca86c659f2a664e
KIMSEI1124 <workju1124@gmail.com>
2023-11-11 22:33:26 +0900

    Chore: Spring Boot Init
    스프링 부트 프로젝트 생성

---
+++ src/test/java/com/goldmans/findmyhomewithsubway/FindmyhomewithsubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldmans.findmyhomewithsubway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmyhomewithsubwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
