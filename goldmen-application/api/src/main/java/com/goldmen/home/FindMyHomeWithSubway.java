db49c97acd4ea3fe1e42ca30aca9ce5d3de243e7
KIMSEI1124 <workju1124@gmail.com>
2023-11-15 10:45:13 +0900

    Chore(#1): application layer module 구성
    gradle 를 수정하고 api와 batch 모듈을 생성

--- goldmen-application/api/src/main/java/com/goldmen/home/FindMyHomeWithSubway.java
+++ goldmen-application/api/src/main/java/com/goldmen/home/FindMyHomeWithSubway.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FindMyHomeWithSubway {
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "application,application-batch,application-core");
         SpringApplication.run(FindMyHomeWithSubway.class);
     }
 }
