06f2a2f5c78a6cfa17e660c55152292951c46ca5
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-11-16 14:28:44 +0900

    Feat(#3):서울공공데이터 집 FetchAPI

---
+++ goldmen-application/goldmen-batch/src/test/java/com/goldmen/home/BatchApplicationTest.java
@@ -0,0 +1,9 @@
+package com.goldmen.home;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BatchApplicationTest {
+}
