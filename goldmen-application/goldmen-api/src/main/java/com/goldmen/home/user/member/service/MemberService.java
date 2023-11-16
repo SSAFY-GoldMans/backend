dad3792bd5bb509367a96e1bd98fd3eb5ec67541
KIMSEI1124 <workju1124@gmail.com>
2023-11-16 16:58:29 +0900

    Feat(#12): ApiResponse 구현

--- goldmen-application/goldmen-api/src/main/java/com/goldmen/home/user/member/service/MemberService.java
+++ goldmen-application/goldmen-api/src/main/java/com/goldmen/home/user/member/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.goldmen.home.user.member.service;
 
+import com.goldmen.home.type.ApiResponse;
 import com.goldmen.home.user.member.domain.Member;
 import com.goldmen.home.user.member.dto.request.MemberSignupRequest;
 import com.goldmen.home.user.member.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.goldmen.home.user.message.MemberMessage.SUCCESS_SIGNUP;
 
 @RequiredArgsConstructor
 @Service
@@ -14,9 +17,12 @@ public class MemberService {
 
     private final MemberMapper mapper;
 
-    public void signup(MemberSignupRequest request) {
+    public ApiResponse<String> signup(MemberSignupRequest request) {
         Member member = mapper.toJpaEntity(request);
 
         memberSaveService.save(member);
+
+        return ApiResponse.noContent()
+                .addMessage(SUCCESS_SIGNUP);
     }
 }
