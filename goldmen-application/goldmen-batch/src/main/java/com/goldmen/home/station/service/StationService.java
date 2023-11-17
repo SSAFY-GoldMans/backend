9774f4c0c7c68cb04900bfac3d7c78cb5293e82c
최우석 <chldn323@gmail.com>
2023-11-17 17:57:46 +0900

    Feat(#17): 지하철 저장 서비스 구현 과정

--- goldmen-application/goldmen-batch/src/main/java/com/goldmen/home/station/service/StationService.java
+++ goldmen-application/goldmen-batch/src/main/java/com/goldmen/home/station/service/StationService.java
@@ -1,6 +1,12 @@
 package com.goldmen.home.station.service;
 
+import com.goldmen.home.dto.request.KaKaoKeywordAPIRequest;
+import com.goldmen.home.metro.line.domain.Line;
+import com.goldmen.home.metro.line.service.LineServiceImpl;
+import com.goldmen.home.metro.station.service.StationServiceImpl;
+import com.goldmen.home.service.KakaoMapService;
 import com.goldmen.home.station.vo.StationInfo;
+import com.goldmen.home.vo.Position;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +17,24 @@ import java.util.List;
 @Service
 public class StationService {
     private SeoulOpenDataStationClient stationClient;
+    private KakaoMapService kakaoService;
+    private StationServiceImpl stationService;
+    private LineServiceImpl lineService;
 
-    public List<StationInfo> getStationInformation(String path) throws IOException {
-        return stationClient.getStationInformationFile(path);
+    private List<StationInfo> getStationInformation() throws IOException {
+        return stationClient.getStationInformationFile();
+    }
+
+    private Position getPositionByKeyword(String keyword) {
+        KaKaoKeywordAPIRequest request = KaKaoKeywordAPIRequest.builder().keyword(keyword).build();
+        return kakaoService.getPosition(request);
+    }
+
+    public void saveStation() throws IOException {
+        List<StationInfo> stationInfoList = getStationInformation();
+        for (StationInfo stationInfo : stationInfoList) {
+            Line line = lineService.find(Line.builder().name(stationInfo.getStationName()).build());
+        }
+        /// TODO: 2023-11-17 stationService.saveStation();
     }
 }
