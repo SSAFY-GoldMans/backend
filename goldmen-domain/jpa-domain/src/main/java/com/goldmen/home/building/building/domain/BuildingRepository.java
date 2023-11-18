c0650c9f3cf5c9c6e7f4006e5f765029713a93a3
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-11-18 17:38:41 +0900

    Feat(#25):Building 모듈 Respository, Service 구현

--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/building/domain/BuildingRepository.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/building/domain/BuildingRepository.java
@@ -1,6 +1,22 @@
 package com.goldmen.home.building.building.domain;
 
+import com.goldmen.home.building.building.data.cond.FindBuildingOptionCond;
+import com.goldmen.home.map.legal.domain.Legal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BuildingRepository extends JpaRepository<Building, Integer> {
+
+    @Query(value = "select b from Building b " +
+            "where b.legal.id = :#{#optionCond.legal.id} " +
+            "and b.lat = :#{#optionCond.lat} " +
+            "and b.lng = :#{#optionCond.lng} " +
+            "and b.mainNumber = :#{#optionCond.mainNumber} " +
+            "and b.subNumber = :#{#optionCond.subNumber} " +
+            "and b.type = :#{#optionCond.buildingType} " +
+            "and b.name = :#{#optionCond.buildingName}")
+    Optional<Building> findFirstByOption(@Param("optionCond") FindBuildingOptionCond optionCond);
 }
