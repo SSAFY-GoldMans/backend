511c997c5873f9bc6eac18909856cc8b1cff8e84
ChoiUS <chldn323@gmail.com>
2023-11-19 23:32:33 +0900

    Feat(#31): findLineByName 서비스 추가

--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/metro/line/service/LineLoadService.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/metro/line/service/LineLoadService.java
@@ -3,5 +3,5 @@ package com.goldmen.home.metro.line.service;
 import com.goldmen.home.metro.line.domain.Line;
 
 public interface LineLoadService {
-    Line find(Line line);
+    Line findByName(Line line);
 }
