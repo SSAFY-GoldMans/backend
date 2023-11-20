4ecb372472546e05d6b4ff8484fc78fa8c606380
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-11-20 16:08:41 +0900

    Feat(#38):역 근처 집 조회

--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/building/domain/BuildingRepository.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/building/domain/BuildingRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BuildingRepository extends JpaRepository<Building, Integer> {
@@ -19,4 +20,11 @@ public interface BuildingRepository extends JpaRepository<Building, Integer> {
             "and b.type = :#{#optionCond.buildingType} " +
             "and b.name = :#{#optionCond.buildingName}")
     Optional<Building> findFirstByOption(@Param("optionCond") FindBuildingOptionCond optionCond);
+
+    //위치 기준으로 dist내에 있는 모든 건물 구하기
+    @Query(value = "select b.* " +
+            "from building b " +
+            "where ST_Distance_Sphere(Point(:lng, :lat), POINT(b.lng, b.lat)) <= :dist " +
+            "order by ST_Distance_Sphere(Point(:lng, :lat), POINT(b.lng, b.lat))", nativeQuery = true)
+    List<Building> findAllByLocationAndDist(@Param("lat") double lat, @Param("lng") double lng, @Param("dist") int dist);
 }
--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/building/service/BuildingService.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/building/service/BuildingService.java
@@ -3,9 +3,12 @@ package com.goldmen.home.building.building.service;
 import com.goldmen.home.building.building.data.cond.FindBuildingOptionCond;
 import com.goldmen.home.building.building.domain.Building;
 import com.goldmen.home.building.building.domain.BuildingRepository;
+import com.goldmen.home.metro.station.domain.Station;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -21,5 +24,14 @@ public class BuildingService {
 
     public Building findById(Building building){
         return buildingRepository.findById(building.getId()).orElseThrow();
+    }
+
+    /**
+     * 역 주변 건물 목록 조회, 800M 기준(걸어서 10분거리)
+     * @param station {@link Station}
+     * @return {@link Building}
+     */
+    public List<Building> findALlByStation(Station station){
+        return buildingRepository.findAllByLocationAndDist(station.getLat(),station.getLng(),800);
     }
 }
