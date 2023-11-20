7a998db5c1ef080628a83ebc3b228a02f9a3f065
ChoiUS <chldn323@gmail.com>
2023-11-20 20:43:40 +0900

    Feat(#41): 가까운 역 찾는 repository 구현

--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/metro/duration/domain/DurationRepository.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/metro/duration/domain/DurationRepository.java
@@ -1,6 +1,15 @@
 package com.goldmen.home.metro.duration.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DurationRepository extends JpaRepository<Duration, Integer> {
+    @Query("select d " +
+            "from Duration d " +
+            "where :time >= d.time " +
+            "AND (d.endStation.id = :station_id OR d.startStation.id = :station_id)")
+    List<Duration> findNearStationByStationIdAndTime(@Param("station_id") int stationId, @Param("time") int time);
 }
