6e6ae9238a9900da5e5f008663d98ceadac0d54c
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-11-22 09:13:40 +0900

    Feat(#42) : 빌딩으로 월세찾기

--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/Monthly/domain/MonthlyRepository.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/Monthly/domain/MonthlyRepository.java
@@ -1,10 +1,24 @@
 package com.goldmen.home.building.Monthly.domain;
 
+import com.goldmen.home.building.Monthly.cond.FindAllCondition;
+import com.goldmen.home.building.building.domain.Building;
 import com.goldmen.home.building.global.domain.HouseInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MonthlyRepository extends JpaRepository<Monthly, Integer> {
     Optional<Monthly> findByHouseInfo(HouseInfo houseInfo);
+
+    List<Monthly> findAllByBuilding(Building building);
+
+    @Query(value = "select m from Monthly m join fetch m.building b join fetch b.legal l join fetch l.district " +
+            "where m.building = :#{#building} " +
+            "and :#{#cond.price.min} <= m.price and m.price<= :#{#cond.price.max} " +
+            "and :#{#cond.area.min} <= m.houseInfo.area and m.houseInfo.area <= :#{#cond.area.max} " +
+            "and :#{#cond.price.min} <= m.price and m.price <= :#{#cond.price.max} order by m.price")
+    List<Monthly> findAllByBuildingAndCond(@Param("building") Building building, @Param("cond") FindAllCondition cond);
 }
--- goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/Monthly/service/MonthlyService.java
+++ goldmen-domain/jpa-domain/src/main/java/com/goldmen/home/building/Monthly/service/MonthlyService.java
@@ -1,10 +1,15 @@
 package com.goldmen.home.building.Monthly.service;
 
+import com.goldmen.home.building.Monthly.cond.FindAllCondition;
 import com.goldmen.home.building.Monthly.domain.Monthly;
 import com.goldmen.home.building.Monthly.domain.MonthlyRepository;
+import com.goldmen.home.building.building.domain.Building;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +25,9 @@ public class MonthlyService {
 
     public Monthly findById(Monthly monthly){
         return monthlyRepository.findById(monthly.getId()).orElseThrow();
+    }
+
+    public List<Monthly> findAllByBuilding(Building building, FindAllCondition condition) {
+        return monthlyRepository.findAllByBuildingAndCond(building,condition);
     }
 }
